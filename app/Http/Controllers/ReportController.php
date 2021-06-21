6d7d3caafed2578a6b39f628aaab0296f12543cc
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-06-21 23:40:40 +0000

    return reports base on resource

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+use App\Models\Sales;
+use App\Models\Product;
+use App\Models\Purchase;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
@@ -14,6 +18,25 @@ class ReportController extends Controller
     }
 
     public function getData(Request $request){
-        
+        $this->validate($request,[
+            'from_date'=>'required',
+            'to_date'=>'required',
+            'resource'=>'required',
+        ]);
+        if ($request->resource == 'sales'){
+            $sales = Sales::whereDate('created_at','=',Carbon::now())->get();
+            $title = "Sales Reports";
+            return view('reports',compact('sales','title'));
+        }
+        if($request->resource == "products"){
+            $title = "Products Reports";
+            $products = Product::get();
+            return view('reports',compact('title','products'));
+        }
+        if($request->resource == 'purchases'){
+            $title = "Purchases Reports";
+            $purchases = Purchase::get();
+            return view('reports',compact('title','purchases'));
+        }
     }
 }
