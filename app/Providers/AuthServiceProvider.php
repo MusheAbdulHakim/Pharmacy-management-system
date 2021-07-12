32db2ea830801d5d3651393ef111bed585ec4cc2
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-07-12 01:28:58 +0000

    super admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,7 +25,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Gate::before(function ($user, $ability) {
-            return $user->hasRole('Super-admin') ? true : null;
+            return $user->hasRole('super-admin') ? true : null;
         });
     }
 }
