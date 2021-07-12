78f04d5987392556a95192f9b9ac52f058ea8239
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-07-12 15:16:12 +0000

    rolepermission seeder

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -9,7 +9,7 @@ use Spatie\Permission\PermissionRegistrar;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
-    
+
     /**
      * Run the database seeds.
      *
@@ -21,14 +21,30 @@ class RolesAndPermissionsSeeder extends Seeder
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
         //create permissions
-        Permission::create(['name' => 'view-sales']);
-        Permission::create(['name' => 'view-reports']);
-        Permission::create(['name' => 'create-sales']);
 
-        // or may be done by chaining
+
+        $arrayOfPermissionNames = [
+          'view-sales', 'create-sales','destroy-sale','update-sales',
+          'view-reports','view-category','create-category','destroy-category','update-category',
+          'view-products','create-product','update-product','destroy-product',
+          'view-purchase','create-purchase','update-purchase','destroy-purchase',
+          'view-supplier','create-supplier','update-supplier','destroy-supplier',
+          'view-users','create-user','update-user','destroy-user',
+          'view-access-control',
+          'view-role','update-role','destroy-role','create-role',
+          'view-permission','create-permission','update-permission','destroy-permission',
+          'view-expired-products','view-outstock-products','backup-app','backup-db','view-settings',
+
+        ];
+       $permissions = collect($arrayOfPermissionNames)->map(function ($permission) {
+           return ['name' => $permission, 'guard_name' => 'web'];
+       });
+
+      Permission::insert($permissions->toArray());
+
+        // create roles and assign permissions
         $role = Role::create(['name' => 'sales-person'])
          ->givePermissionTo(['view-sales', 'view-reports','create-sales']);
-
         $role = Role::create(['name' => 'super-admin']);
         $role->givePermissionTo(Permission::all());
     }
