056c2114e94c14f503d3a2127d0793b0daf00f7c
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-08-07 06:50:35 -0400

    default timezone changed

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE','UTC'),
+    'timezone' => env('APP_TIMEZONE','GMT'),
 
     /*
     |--------------------------------------------------------------------------
