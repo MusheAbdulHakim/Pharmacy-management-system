a3cdaebc5c9ee770e3f1ed40c4f4850cd4218189
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-08-10 13:36:11 +0000

    timezone reverted back

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE','GMT'),
+    'timezone' => env('APP_TIMEZONE','UTC'),
 
     /*
     |--------------------------------------------------------------------------
