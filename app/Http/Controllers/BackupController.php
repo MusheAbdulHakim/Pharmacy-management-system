254af1e629e3cef52643b4874968b5c0496eb71e
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-10-13 23:14:28 +0000

    backup controller

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -2,28 +2,132 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
+use Log;
+use Artisan;
+use Request;
+use Storage;
+use Response;
+use Exception;
+use League\Flysystem\Adapter\Local;
 
 class BackupController extends Controller
 {
     
-    public function index(){
-        Artisan::call('backup:run --disable-notifications');
-        $notification = [
-            'message'=>"Backup has been made",
-            'alert-type'=>'success',
-        ];
-        return redirect()->route('dashboard')->with($notification);
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $title = 'backups';
+        if (!count(config('backup.backup.destination.disks'))) {
+            dd(trans('backup.no_disks_configured'));
+        }
+        $this->data['backups'] = [];
+        foreach (config('backup.backup.destination.disks') as $disk_name) {
+            $disk = Storage::disk($disk_name);
+            $adapter = $disk->getDriver()->getAdapter();
+            $files = $disk->allFiles();
+
+            // make an array of backup files, with their filesize and creation date
+            foreach ($files as $k => $f) {
+                // only take the zip files into account
+                if (substr($f, -4) == '.zip' && $disk->exists($f)) {
+                    $this->data['backups'][] = [
+                        'file_path'     => $f,
+                        'file_name'     => str_replace('backups/', '', $f),
+                        'file_size'     => $disk->size($f),
+                        'last_modified' => $disk->lastModified($f),
+                        'disk'          => $disk_name,
+                        'download'      => ($adapter instanceof Local) ? true : false,
+                    ];
+                }
+            }
+        }
+        return view('backups',$this->data,compact(
+            'title',
+        ));
+        
     }
 
-    public function database(){
-        Artisan::call('backup:run --only-db --disable-notifications');
-        $notification = [
-            'message'=>"Database has been backed up",
-            'alert-type'=>'success',
-        ];
-        return redirect()->route('dashboard')->with($notification);
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $notification = 'backup created successfully';
+        try {
+            ini_set('max_execution_time', 600);
+
+            Log::info('Backpack\BackupManager -- Called backup:run from admin interface');
+
+            Artisan::call('backup:run');
+
+            $output = Artisan::output();
+            if (strpos($output, 'Backup failed because')) {
+                preg_match('/Backup failed because(.*?)$/ms', $output, $match);
+                $notification = notify('backup process failed because ');
+                $notification .= isset($match[1]) ? $match[1] : '';
+                Log::error($notification.PHP_EOL.$output);
+            } else {
+                Log::info("Backpack\BackupManager -- backup process has started");
+            }
+        } catch (Exception $e) {
+            Log::error($e);
+
+            return Response::make($e->getMessage(), 500);
+        }
+
+        return back()->with($notification);
+    }
+
+    
+    
+
+    /**
+     * Downloads a backup zip file.
+     */
+    public function download()
+    {
+        $disk = Storage::disk(Request::input('disk'));
+        $file_name = Request::input('file_name');
+        $adapter = $disk->getDriver()->getAdapter();
+
+        if ($adapter instanceof Local) {
+            $storage_path = $disk->getDriver()->getAdapter()->getPathPrefix();
+
+            if ($disk->exists($file_name)) {
+                return response()->download($storage_path.$file_name);
+            } else {
+                abort(404, trans('backup.backup_doesnt_exist'));
+            }
+        } else {
+            abort(404, trans('backup.only_local_downloads_supported'));
+        }
+    }
+
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  file $file_name
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($file_name)
+    {
+        $disk = Storage::disk(Request::input('disk'));
+
+        if ($disk->exists($file_name)) {
+            $disk->delete($file_name);
+            $notification = notify('backup created successfully');
+            return back()->with($notification);
+        } else {
+            abort(404, trans('backup.backup_doesnt_exist'));
+        }
     }
 
 }
