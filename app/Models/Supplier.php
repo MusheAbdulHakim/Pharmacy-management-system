b416b2e4e7decaa164e0e3dfea629863d0bd8000
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-02-09 16:11:09 +0000

    supplier model

---
+++ app/Models/Supplier.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name','email','phone','company',
+        'address','product','comment'
+    ];
+}
