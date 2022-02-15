3aa6fca7f5551f1b153ae0c3f5bd80921705f44e
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-02-15 17:56:27 +0000

    create purchase form

---
+++ resources/views/admin/purchases/create.blade.php
@@ -0,0 +1,111 @@
+@extends('admin.layouts.app')
+
+@push('page-css')
+	<!-- Datetimepicker CSS -->
+	<link rel="stylesheet" href="{{asset('assets/css/bootstrap-datetimepicker.min.css')}}">
+@endpush
+
+@push('page-header')
+<div class="col-sm-12">
+	<h3 class="page-title">Add Purchase</h3>
+	<ul class="breadcrumb">
+		<li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
+		<li class="breadcrumb-item active">Add Purchase</li>
+	</ul>
+</div>
+@endpush
+
+
+@section('content')
+<div class="row">
+	<div class="col-sm-12">
+		<div class="card">
+			<div class="card-body custom-edit-service">
+				
+				<!-- Add Medicine -->
+				<form method="post" enctype="multipart/form-data" autocomplete="off" action="{{route('purchases.store')}}">
+					@csrf
+					<div class="service-fields mb-3">
+						<div class="row">
+							<div class="col-lg-4">
+								<div class="form-group">
+									<label>Medicine Name<span class="text-danger">*</span></label>
+									<input class="form-control" type="text" name="product" >
+								</div>
+							</div>
+							<div class="col-lg-4">
+								<div class="form-group">
+									<label>Category <span class="text-danger">*</span></label>
+									<select class="select2 form-select form-control" name="category"> 
+										@foreach ($categories as $category)
+											<option value="{{$category->id}}">{{$category->name}}</option>
+										@endforeach
+									</select>
+								</div>
+							</div>
+							<div class="col-lg-4">
+								<div class="form-group">
+									<label>Supplier <span class="text-danger">*</span></label>
+									<select class="select2 form-select form-control" name="supplier"> 
+										@foreach ($suppliers as $supplier)
+											<option value="{{$supplier->id}}">{{$supplier->name}}</option>
+										@endforeach
+									</select>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					<div class="service-fields mb-3">
+						<div class="row">
+							<div class="col-lg-6">
+								<div class="form-group">
+									<label>Cost Price<span class="text-danger">*</span></label>
+									<input class="form-control" type="text" name="cost_price">
+								</div>
+							</div>
+							<div class="col-lg-6">
+								<div class="form-group">
+									<label>Quantity<span class="text-danger">*</span></label>
+									<input class="form-control" type="text" name="quantity">
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="service-fields mb-3">
+						<div class="row">
+							<div class="col-lg-6">
+								<div class="form-group">
+									<label>Expire Date<span class="text-danger">*</span></label>
+									<input class="form-control" type="date" name="expiry_date">
+								</div>
+							</div>
+							<div class="col-lg-6">
+								<div class="form-group">
+									<label>Medicine Image</label>
+									<input type="file" name="image" class="form-control">
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					
+					<div class="submit-section">
+						<button class="btn btn-primary submit-btn" type="submit" >Submit</button>
+					</div>
+				</form>
+				<!-- /Add Medicine -->
+
+			</div>
+		</div>
+	</div>			
+</div>
+@endsection
+
+@push('page-js')
+	<!-- Datetimepicker JS -->
+	<script src="{{asset('assets/js/moment.min.js')}}"></script>
+	<script src="{{asset('assets/js/bootstrap-datetimepicker.min.js')}}"></script>	
+@endpush
+
